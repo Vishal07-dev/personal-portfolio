a6144616669862066788ff313a7afd82e22434f4
vishal-07dev <vishalrohera07@gmail.com>
2024-09-16 07:23:16 +0530

    Bug solved

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -29,7 +29,7 @@ const NavBar = () => {
         <div className="flex justify-between items-center w-full h-20 text-white
      bg-black px-4 fixed">
             <div>
-                <h1 className="text-5xl font-signature">Nony</h1>
+                <h1 className="text-5xl font-signature">Vishal</h1>
             </div>
             <ul className="hidden md:flex">
                 {link.map(({ id, link }) => (
