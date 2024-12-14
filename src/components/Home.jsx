bc262ec64dbb4123f791f5bbf3f1edf62eac92c4
vishal-07dev <vishalrohera07@gmail.com>
2024-12-14 17:48:47 +0530

    the image changed

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,9 +5,9 @@ import { Link } from "react-scroll";
 const Home = () => {
     return (
         <div name="Home" className="h-screen w-full bg-gradient-to-b from-black via-black to-gray-800">
-            <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center 
-        h-full px-4 md:flex-row">
-                <div>
+            <div className=" max-w-screen-lg mx-auto flex flex-col items-center justify-center 
+        h-full px-4 md:flex-row gap-14">
+                <div className='mt-28 md"mt-0'>
                     <h2 className="text-4xl sm:text-5xl font-bold text-white">I am <span className='text-red-500'>Vishal Rohera</span> , a Web Developer</h2>
                     <p className="text-gray-500 py-4 max-w-md">
                     </p>
@@ -20,7 +20,7 @@ const Home = () => {
                         </Link>
                     </div>
                 </div>
-                <div className='p-4 rounded-md'>
+                <div className='md:px-2 rounded-lg  bg-gray-600 mt-10 md:m-0'>
                     <img src={HeroImage} alt="my pitcure" className="rounded-2xl mx-auto w-2/3 md:w-full" />
                 </div>
             </div>
